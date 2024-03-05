65fe159a1e8c1fdd1739314a1e39739be88bb318
Nirajan Bekoju <nirajan.bekoju@gmail.com>
2024-03-05 14:21:26 +0545

    fix: add division function
    - add all four major arithmetic operation

--- arithmetic.py
+++ arithmetic.py
@@ -7,9 +7,13 @@ def subtraction(x, y):
 def multiplication(x, y):
     return x*y
 
+def division(x, y):
+    return x/y
+
 if __name__=="__main__":
     x = 10
     y = 5
     print(addition(x, y))
     print(subtraction(x, y))
     print(multiplication(x, y))
+    print(division(x, y))
